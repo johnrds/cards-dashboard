b14fb10e696f2b01fc1bf58012650dad38de0c5e
johnrds <121064478+johnrds@users.noreply.github.com>
2023-03-14 14:36:26 -0300

    Improve hidden nav to show icons

--- js/script.js
+++ js/script.js
@@ -7,21 +7,7 @@ nav_links.forEach(link => link.addEventListener('click', (e) => {
 
 const nav_toggle = document.querySelector("#nav-toggle");
 const nav_bar = document.querySelector("nav");
-const container = document.querySelector(".container");
-
-// let hide = false;
 
 nav_toggle.addEventListener("click", (e) => {
-
     nav_bar.classList.toggle("hidden");
-    container.classList.toggle("hidden");
-    // if (hide) {
-    //     nav_bar.style.transform = "translateX(0px)"
-    //     container.style.gridTemplateColumns = "minmax(230px, 1fr) 4fr"
-    // } else {
-    //     nav_bar.style.transform = "translateX(-500px)";
-    //     container.style.gridTemplateColumns = "0px 4fr"
-    // }
-
-    // hide = !hide;
 })
